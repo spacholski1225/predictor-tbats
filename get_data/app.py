8068d5e64e099b7a5adb1775b241ee6e0cb69edd
spacholski <spacholski99@gmail.com>
2025-04-29 17:53:48 +0200

    Removed comment

--- get_data/app.py
+++ get_data/app.py
@@ -4,7 +4,7 @@ import os
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)  # Enable CORS for all routes
+CORS(app) 
 
 @app.route('/getUnpredictedData', methods=['GET'])
 def get_unpredicted_data():
